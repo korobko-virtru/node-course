0f0d1deb0fa13194b991c4668cad146149d4da68
Alex Korobko <okorobka@virtru.com>
2021-11-10 17:50:25 +0200

    Made suggested changes

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,16 @@
 import {createServer} from 'http';
 
-createServer((req, res) => {
+createServer(async (req, res) => {
     if (req.url === '/' && req.method === 'GET') {
-        res.end('Home Page');
-        return;
+        return res.end('Home Page');
     } else if (req.url === '/add-user' && req.method === 'POST') {
         const chunkArray: any = [];
-        req.on('data', (chunk) => {
+        for await (const chunk of req) {
             chunkArray.push(...chunk);
-        });
-        req.on('end', () => {
-            const data = Buffer.from(chunkArray).toString();
-            res.end(data);
-            return;
-        });
-        req.on('error', () => {
-            res.end('Error reading request data');
-            return;
-        })
+        }
+        const data = Buffer.from(chunkArray).toString();
+
+        return res.end(data);
     } else {
         res.end('Unhandled Path');
     }
